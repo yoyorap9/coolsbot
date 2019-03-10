362c799b16820b36ec1007119d5a5fd679960f4d
yoyorap9 <48361489+yoyorap9@users.noreply.github.com>
2019-03-10 12:03:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,13 +31,12 @@ client.on('ready', () => {
         console.log(" - " + guild.name)
     })
 })
-client.on('ready', function(message){
+client.on('message', function(message){
 	if(message.content === (prefix + 'servers')){
     	message.channel.send("Servers:")
     	client.guilds.forEach((guild) => {
         console.log(" - " + guild.name)
-        })
-    })	 
+        }})	 
 })
 client.on('message', function(message){
 		if(message.content === ('Trol')){
